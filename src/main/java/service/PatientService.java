1630a2c1193786fd50df91dbc149f968f2f970a9
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 12:07:11 +0330

    add addPatient method

--- src/main/java/service/PatientService.java
+++ src/main/java/service/PatientService.java
@@ -1,8 +1,10 @@
 package service;
 
+import entities.Patient;
 import repository.PatientRepository;
 import utilities.Validation;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class PatientService {
@@ -13,11 +15,21 @@ public class PatientService {
         this.patientRepository = patientRepository;
     }
 
-    public void addPatient (){
+    public void addPatient () throws SQLException {
         System.out.println("****** patient info ******");
         System.out.println("Enter name : ");
         String name = scanner.nextLine();
         String nationalId = getValidNationalId();
+        System.out.println("Enter username : ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password : ");
+        String password = scanner.nextLine();
+
+        int result = patientRepository.save(new Patient(name, nationalId, username, password));
+        if (result!=0)
+            System.out.println("successfull-Patient is added");
+        else
+            System.out.println("Error-Patient is not added");
     }
 
     private String getValidNationalId() {
