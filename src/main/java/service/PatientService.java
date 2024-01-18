db4eaf396ff14e2c238579f07481b57897d2d714
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 12:00:21 +0330

    add getValidNationalId method

--- src/main/java/service/PatientService.java
+++ src/main/java/service/PatientService.java
@@ -1,11 +1,34 @@
 package service;
 
 import repository.PatientRepository;
+import utilities.Validation;
+
+import java.util.Scanner;
 
 public class PatientService {
     private final PatientRepository patientRepository ;
+    private final Scanner scanner = new Scanner(System.in);
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public void addPatient (){
+        System.out.println("****** patient info ******");
+        System.out.println("Enter name : ");
+        String name = scanner.nextLine();
+        String nationalId = getValidNationalId();
+    }
+
+    private String getValidNationalId() {
+        String nationalId = "" ;
+        while (true) {
+            System.out.println("Enter national id : ");
+            nationalId = scanner.nextLine();
+            if (Validation.validateMelliCode(nationalId))
+                return nationalId;
+            else
+                System.out.println("invalid national id");
+        }
     }
 }
