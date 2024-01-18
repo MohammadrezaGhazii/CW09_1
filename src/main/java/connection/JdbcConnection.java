813c03b040fff64385217b661bfddfbd1e458da0
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:32:12 +0330

    add connection field and getter

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,21 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    private static final Connection CONNECTION;
+
+    static {
+        try {
+            CONNECTION = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cw09_1",
+                    "postgres", "lplnvqh81");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static Connection getConnection (){
+        return CONNECTION;
+    }
 }
