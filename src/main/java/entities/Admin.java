228a5b6bdc6a1cc802121ee97de00ae502a0f472
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:06:00 +0330

    add constructors

--- src/main/java/entities/Admin.java
+++ src/main/java/entities/Admin.java
@@ -6,5 +6,19 @@ public class Admin {
     private String username ;
     private String password ;
 
+    public Admin() {
+    }
 
+    public Admin(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Admin(int adminId, String name, String username, String password) {
+        this.adminId = adminId;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
 }
