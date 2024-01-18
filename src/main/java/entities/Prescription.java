323cea0c23a5d147f9b5b066d1005ce460e152b0
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 10:55:12 +0330

    add needed fields

--- src/main/java/entities/Prescription.java
+++ src/main/java/entities/Prescription.java
@@ -1,4 +1,12 @@
 package entities;
 
+import java.math.BigDecimal;
+
 public class Prescription {
+    private int id;
+    private int patientId;
+    private String medicine;
+    private int number;
+    private boolean doesExist;
+    private BigDecimal price;
 }
