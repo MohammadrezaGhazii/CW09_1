723f9deaf9d63b181c9cbbd756536f3dcf30b85e
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 10:51:58 +0330

    add needed fields

---
+++ src/main/java/entities/Patient.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Patient {
+    private int patientID ;
+    private String patientName ;
+    private String nationalId ;
+    private String username ;
+    private String password ;
+    private Prescription [] prescription ;
+
+    public Patient() {
+    }
+
+}
