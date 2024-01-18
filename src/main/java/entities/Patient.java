77f7e2e407cdd1cb5c5eeca0c10f4f5ae4e9c590
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:03:44 +0330

    add equals and hashcode

--- src/main/java/entities/Patient.java
+++ src/main/java/entities/Patient.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Patient {
     private int patientID ;
@@ -87,5 +88,20 @@ public class Patient {
                 ", password='" + password + '\'' +
                 ", prescription=" + Arrays.toString(prescription) +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Patient patient = (Patient) o;
+        return patientID == patient.patientID && Objects.equals(patientName, patient.patientName) && Objects.equals(nationalId, patient.nationalId) && Objects.equals(username, patient.username) && Objects.equals(password, patient.password) && Arrays.equals(prescription, patient.prescription);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(patientID, patientName, nationalId, username, password);
+        result = 31 * result + Arrays.hashCode(prescription);
+        return result;
     }
 }
