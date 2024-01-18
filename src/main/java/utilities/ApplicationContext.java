b903b526a5a1264df7788dd0f1963666501aa69e
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 15:02:06 +0330

    add needed fields and new patient repository and services objects

--- src/main/java/utilities/ApplicationContext.java
+++ src/main/java/utilities/ApplicationContext.java
@@ -1,4 +1,21 @@
 package utilities;
 
+import connection.JdbcConnection;
+import repository.PatientRepository;
+import service.PatientService;
+
+import java.sql.Connection;
+
 public class ApplicationContext {
+    private static final Connection CONNECTION ;
+    private static final PatientRepository PATIENT_REPOSITORY ;
+    private static final PatientService PATIENT_SERVICE ;
+    static {
+        CONNECTION = JdbcConnection.getConnection();
+        PATIENT_REPOSITORY = new PatientRepository(CONNECTION);
+        PATIENT_SERVICE = new PatientService(PATIENT_REPOSITORY);
+    }
+    public static PatientService getPatientService(){
+        return PATIENT_SERVICE ;
+    }
 }
