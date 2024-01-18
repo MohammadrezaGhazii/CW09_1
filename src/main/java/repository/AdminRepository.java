0378f6cb343a12b178d7bcb6bb41aeddb828b2ce
arianaaghajani <aghajaniariana@gmail.com>
2024-01-18 16:43:06 +0330

    add connection filed and constructor

--- src/main/java/repository/AdminRepository.java
+++ src/main/java/repository/AdminRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import java.sql.Connection;
+
 public class AdminRepository {
+    private final Connection connection;
+
+
+    public AdminRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
