584e0d530258418b2f12a16ceb26bcedf4c698f2
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:06:21 +0330

    add getter and setter

--- src/main/java/entities/Admin.java
+++ src/main/java/entities/Admin.java
@@ -21,4 +21,36 @@ public class Admin {
         this.username = username;
         this.password = password;
     }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
