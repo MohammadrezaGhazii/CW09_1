a9827107c59fb5bc2b88b02754407e53f0874aa7
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 16:31:32 +0330

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import service.PatientService;
+import utilities.ApplicationContext;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        PatientService patientService = ApplicationContext.getPatientService();
+        System.out.println("Enter id : ");
+        int id = scanner.nextInt();
+        patientService.deletePatient(id);
 
     }
 }
