33c3b5a7c1445b69d0db4942a9a17fd2dedf1360
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:34:46 +0330

    add connection field and constructor

--- src/main/java/repository/PatientRepository.java
+++ src/main/java/repository/PatientRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class PatientRepository {
+    private final Connection connection ;
+
+    public PatientRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
