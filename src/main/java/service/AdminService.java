6d2628ea3ae3e4b2bf6629326891332cf226c2f6
arianaaghajani <aghajaniariana@gmail.com>
2024-01-18 16:59:34 +0330

    add AdminRepository field and constructor

--- src/main/java/service/AdminService.java
+++ src/main/java/service/AdminService.java
@@ -1,4 +1,11 @@
 package service;
 
+import repository.AdminRepository;
+
 public class AdminService {
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
 }
