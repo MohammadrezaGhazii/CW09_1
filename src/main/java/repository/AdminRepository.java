e8db10fe9ba0e93cf1c68c8515b133e2e7b86bfa
arianaaghajani <aghajaniariana@gmail.com>
2024-01-18 16:56:49 +0330

    add save method

--- src/main/java/repository/AdminRepository.java
+++ src/main/java/repository/AdminRepository.java
@@ -1,6 +1,10 @@
 package repository;
 
+import entities.Admin;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class AdminRepository {
     private final Connection connection;
@@ -8,5 +12,14 @@ public class AdminRepository {
 
     public AdminRepository(Connection connection) {
         this.connection = connection;
+    }
+    public int save(Admin admin) throws SQLException {
+        String saveQuery="INSERT INTO admin(name, username, password) VALUES(?,?,?);";
+        PreparedStatement ps=connection.prepareStatement(saveQuery);
+        ps.setString(1, admin.getName());
+        ps.setString(2,admin.getUsername());
+        ps.setString(3,admin.getPassword());
+
+        return ps.executeUpdate();
     }
 }
