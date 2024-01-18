32014fc79c75176db93cf2beaa9ea9664efafef5
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:49:10 +0330

    add field and constructors

--- src/main/java/service/PatientService.java
+++ src/main/java/service/PatientService.java
@@ -1,4 +1,11 @@
 package service;
 
+import repository.PatientRepository;
+
 public class PatientService {
+    private final PatientRepository patientRepository ;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
 }
