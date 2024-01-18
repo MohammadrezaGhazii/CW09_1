3738c56792f64358de180bf54aa163a6a7473a3a
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-18 11:02:16 +0330

    add constructors

--- src/main/java/entities/Patient.java
+++ src/main/java/entities/Patient.java
@@ -11,4 +11,19 @@ public class Patient {
     public Patient() {
     }
 
+    public Patient(String patientName, String nationalId, String username, String password) {
+        this.patientName = patientName;
+        this.nationalId = nationalId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Patient(int patientID, String patientName, String nationalId, String username, String password, Prescription[] prescription) {
+        this.patientID = patientID;
+        this.patientName = patientName;
+        this.nationalId = nationalId;
+        this.username = username;
+        this.password = password;
+        this.prescription = prescription;
+    }
 }
