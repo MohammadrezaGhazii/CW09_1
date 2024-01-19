72a1f1f4d8927f1e1c857efafc1bffa9edd0b01d
samangishamim <samangi624@gmail.com>
2024-01-19 12:02:54 +0330

    test confirmByAdmin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import service.AdminService;
 import service.PatientService;
+import service.PrescriptionService;
 import utilities.ApplicationContext;
 
 import java.math.BigDecimal;
@@ -13,9 +14,14 @@ public class Main {
 //        System.out.println("Enter id : ");
 //        int id = scanner.nextInt();
 //        patientService.deletePatient(id);
-        AdminService adminService=ApplicationContext.getAdminService();
-        adminService.addAdmin();
+//        AdminService adminService=ApplicationContext.getAdminService();
+//        adminService.addAdmin();
+//        PrescriptionService prescriptionService=ApplicationContext.getPrescriptionService();
+//        prescriptionService.addPrescription(1);
 
-
+//        PatientService patientService=ApplicationContext.getPatientService();
+//        patientService.addPatient();
+        PrescriptionService prescriptionService=ApplicationContext.getPrescriptionService();
+        prescriptionService.confirmByAdmin(1);
     }
 }
