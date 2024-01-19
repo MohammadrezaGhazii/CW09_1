aa032e1743420c88757406af6183b7b120cd3c51
arianaaghajani <aghajaniariana@gmail.com>
2024-01-19 09:20:28 +0330

    patient service and patient repository objects created and getter

--- src/main/java/utilities/ApplicationContext.java
+++ src/main/java/utilities/ApplicationContext.java
@@ -1,7 +1,9 @@
 package utilities;
 
 import connection.JdbcConnection;
+import repository.AdminRepository;
 import repository.PatientRepository;
+import service.AdminService;
 import service.PatientService;
 
 import java.sql.Connection;
@@ -10,12 +12,21 @@ public class ApplicationContext {
     private static final Connection CONNECTION ;
     private static final PatientRepository PATIENT_REPOSITORY ;
     private static final PatientService PATIENT_SERVICE ;
+    private static final AdminRepository ADMIN_REPOSITORY;
+    private static final AdminService ADMIN_SERVICE;
+
     static {
         CONNECTION = JdbcConnection.getConnection();
         PATIENT_REPOSITORY = new PatientRepository(CONNECTION);
         PATIENT_SERVICE = new PatientService(PATIENT_REPOSITORY);
+
+        ADMIN_REPOSITORY=new AdminRepository(CONNECTION);
+        ADMIN_SERVICE=new AdminService(ADMIN_REPOSITORY);
     }
     public static PatientService getPatientService(){
         return PATIENT_SERVICE ;
+    }
+    public static AdminService getAdminService(){
+        return ADMIN_SERVICE;
     }
 }
