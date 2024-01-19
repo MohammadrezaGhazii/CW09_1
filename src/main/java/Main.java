fba9c9ace1b408f404573df3ffaa80e2e932df8f
Parisa-Norouzi <parisanor6@gmail.com>
2024-01-19 15:52:41 +0330

    test show Prescription

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,19 +10,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws SQLException {
 //        Scanner scanner = new Scanner(System.in);
-//        PatientService patientService = ApplicationContext.getPatientService();
+       // PatientService patientService = ApplicationContext.getPatientService();
 //        System.out.println("Enter id : ");
 //        int id = scanner.nextInt();
 //        patientService.deletePatient(id);
 //        AdminService adminService=ApplicationContext.getAdminService();
 //        adminService.addAdmin();
-//        PrescriptionService prescriptionService=ApplicationContext.getPrescriptionService();
-//        prescriptionService.addPrescription(1);
+     //   PrescriptionService prescriptionService=ApplicationContext.getPrescriptionService();
+      //  prescriptionService.addPrescription(1);
 
 //        PatientService patientService=ApplicationContext.getPatientService();
 //        patientService.addPatient();
-        PrescriptionService prescriptionService=ApplicationContext.getPrescriptionService();
-//        prescriptionService.confirmByAdmin(1);
-        prescriptionService.adminConfirmByPatientId(1);
+     PrescriptionService prescriptionService=ApplicationContext.getPrescriptionService();
+    // prescriptionService.confirmByAdmin(1);
+      //prescriptionService.adminConfirmByPatientId(1);
+       // patientService.addPatient();
+        prescriptionService.showPrescription(1);
     }
 }
