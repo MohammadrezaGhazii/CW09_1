deb5c44b7d29e9b0bd03b84989df19409b9ef0b2
samangishamim <samangi624@gmail.com>
2024-01-19 10:28:29 +0330

    add PrescriptionRepository filed

--- src/main/java/service/PrescriptionService.java
+++ src/main/java/service/PrescriptionService.java
@@ -1,6 +1,12 @@
 package service;
 
+import repository.PrescriptionRepository;
+
 public class PrescriptionService {
+    private final PrescriptionRepository prescriptionRepository;
 
 
+    public PrescriptionService(PrescriptionRepository prescriptionRepository) {
+        this.prescriptionRepository = prescriptionRepository;
+    }
 }
