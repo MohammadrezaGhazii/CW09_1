c79e7251cf462b02a3faead4e687ab16f6831789
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-01-19 16:21:25 +0330

    add main menu

--- src/main/java/service/Menu.java
+++ src/main/java/service/Menu.java
@@ -1,4 +1,29 @@
 package service;
 
+import java.util.Scanner;
+
 public class Menu {
+    private final Scanner scanner = new Scanner(System.in);
+    public void menu (){
+        int choice = -1 ;
+        while (choice != 0){
+            System.out.println("*** Main menu ***");
+            System.out.println("1-Admin");
+            System.out.println("2-Patient");
+            System.out.println("0-Exit");
+
+            System.out.println("Choose a number : ");
+            choice = scanner.nextInt();
+
+            switch (choice){
+                case 1 -> System.out.println("admin");
+                case 2 -> System.out.println("Patient");
+                case 0 -> {
+                    System.out.println("Bye Bye");
+                    break;
+                }
+            }
+        }
+
+    }
 }
